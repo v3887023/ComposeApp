7a1b61ec148c390e638d503b46ad64b5a9047356
zhangcx <zhangcx1@yunjiglobal.com>
2021-03-12 15:15:57 +0800

    第一次提交，引入 Jetpack Compose

---
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.composeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.compose.runtime.Composable
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
